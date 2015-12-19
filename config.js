c69cb9a1d959dfee874f2d0673bb1aac1a359ab0
SOMENATH GHOSH <somenathghosh@SOMENATHs-MacBook-Air.local>
2015-12-19 20:51:12 +0530

    added text to IoT

--- config.js
+++ config.js
@@ -11,8 +11,8 @@ config = {
     // When running Ghost in the wild, use the production environment.
     // Configure your URL and mail settings here
     production: {
-        //url: 'https://somenath-ghosh.herokuapp.com/',
-        url: 'http://localhost:3001/',
+        url: 'https://somenath-ghosh.herokuapp.com/',
+        //url: 'http://localhost:3001/',
         mail: {
             transport: 'SMTP',
             options: {
