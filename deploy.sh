6b6cd7a3e79751da8a8688aec7511319f8fd3753
EC2 Default User <ec2-user@ip-172-31-39-28.us-west-2.compute.internal>
2016-04-23 05:21:02 +0000

    DATE_04_23_16_TIME_05_21_02

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 dateTime=`date "+DATE_%m_%d_%y_TIME_%H_%M_%S"`
-echo $dateTime
-output="$(git add . && git commit -m $datetime && git push heroku master)"
-echo $output >> log_$dateTime.log
-#`curl -i http://www.somenathghosh.space` >> log_$dateTime.log
-find /home/ec2-user/log_*.log -mmin +30 -exec rm -f {} \;
-
+#!/bin/bash -e
+commit_message=$dateTime
+git add . -A
+git commit -m "$commit_message"
